3cea95d2ba21e1a671a477d5d608ce9f4b8dfa96
u6lqre <ozwtn9@tutamail.com>
2026-02-13 12:27:33 +0100

    feat(register): add form validation errors

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,26 +1,79 @@
+import { AnimatePresence, motion } from "motion/react";
 import styles from "./styles.module.css";
+import { useForm } from "react-hook-form";
 
-type Props = {};
+type RegisterForm = {
+  name: string;
+  password: string;
+};
 
-function Register({}: Props) {
+function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterForm>();
+
+  const onSubmit = (data: RegisterForm) => console.log(data);
+
+  console.log(errors);
+
   return (
     <div id="centered-content">
       <h1 className={styles.title}>Create an account</h1>
 
-      <form action="#" className={styles.form}>
-        <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          autoComplete="none"
-          className={styles.input}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          className={styles.input}
-        />
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <div className={styles.inputContainer}>
+          <input
+            {...register("name", {
+              required: { value: true, message: "Required" },
+              minLength: { value: 3, message: "Too short" },
+              maxLength: { value: 20, message: "Too long" },
+            })}
+            type="text"
+            placeholder="Name"
+            autoComplete="none"
+            maxLength={20}
+            className={`${styles.input} ${errors.name && styles.error}`}
+          />
+          <AnimatePresence>
+            {errors.name && (
+              <motion.span
+                initial={{ opacity: 0, scale: 0.97 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.97 }}
+                transition={{ type: "spring", duration: 0.2 }}
+                className={styles.errorMessage}
+              >
+                {errors.name.message}
+              </motion.span>
+            )}
+          </AnimatePresence>
+        </div>
+        <div className={styles.inputContainer}>
+          <input
+            {...register("password", {
+              required: { value: true, message: "Required" },
+              minLength: { value: 8, message: "Too short" },
+            })}
+            type="password"
+            placeholder="Password"
+            className={`${styles.input} ${errors.password && styles.error}`}
+          />
+          <AnimatePresence>
+            {errors.password && (
+              <motion.span
+                initial={{ opacity: 0, scale: 0.97 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.97 }}
+                transition={{ type: "spring", duration: 0.2 }}
+                className={styles.errorMessage}
+              >
+                {errors.password.message}
+              </motion.span>
+            )}
+          </AnimatePresence>
+        </div>
 
         <button className={styles.button}>Continue</button>
       </form>
