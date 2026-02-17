1ebbdb6dc6f22bae7eb53aed2fb475c9296a4a1d
u6lqre <ozwtn9@tutamail.com>
2026-02-17 12:11:28 +0100

    styles(register): add button status animation

--- src/features/auth/components/AuthForm/Button.tsx
+++ src/features/auth/components/AuthForm/Button.tsx
@@ -1,13 +1,29 @@
+import { LoaderCircle } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
 import styles from "./Button.module.css";
 
-type Props = {};
+type Props = {
+  isPending: boolean;
+};
 
-function Button({}: Props) {
+function Button({ isPending }: Props) {
   return (
     <button className={styles.button}>
-      <AnimatePresence mode="popLayout">
-        <motion.span>Continue</motion.span>
+      <AnimatePresence mode="popLayout" initial={false}>
+        <motion.span
+          className={styles.span}
+          key={`isPending-${isPending}`}
+          initial={{ y: -25, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: 25, opacity: 0 }}
+          transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+        >
+          {isPending ? (
+            <LoaderCircle size={16} className={styles.loader} />
+          ) : (
+            "Continue"
+          )}
+        </motion.span>
       </AnimatePresence>
     </button>
   );
