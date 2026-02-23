b93e36e19df6e6b157202cde6e192f6522f31abc
u6lqre <ozwtn9@tutamail.com>
2026-02-23 22:30:40 +0100

    feat: add minimun loading time to mutationFn

--- src/features/auth/hooks/useCreateUser.ts
+++ src/features/auth/hooks/useCreateUser.ts
@@ -1,8 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import createUser from "../api/createUser";
+import type { RegisterForm } from "../types";
 
 export default function useCreateUser() {
   return useMutation({
-    mutationFn: createUser,
+    mutationFn: async (data: RegisterForm) => {
+      await sleep();
+      return createUser(data);
+    },
   });
+}
+
+function sleep() {
+  const minLoadingTime = 300;
+  return new Promise((resolve) => setTimeout(resolve, minLoadingTime));
 }
